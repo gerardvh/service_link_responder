633a87a4730ed5d479abf6fe051475517657d691
Gerard Van Halsema <gerardvh@me.com>
2015-05-15 10:44:21 -0400

    Add getQueryString() to request

--- service_link_responder/service_link_request_handler.py
+++ service_link_responder/service_link_request_handler.py
@@ -1,7 +1,8 @@
 import requests
 import json
 
-url = 'https://umichprod.service-now.com/api/now/table/cmdb_ci'
+ciUrl = 'https://umichprod.service-now.com/api/now/table/cmdb_ci'
+incUrl = 'https://umichprod.service-now.com/api/now/table/incident'
 user = '***REMOVED***'
 pwd = '***REMOVED***'
 
@@ -11,4 +12,32 @@ limit = 10
 fieldsToReturn = 'location,serial_number,name'
 displayName = True
 
-headers = {"Accept":"application/json"}
+headers = {"Accept": "application/json"}
+
+def process(query):
+    # addParameters(p)
+    parameters = {}
+    parameters['sysparm_limit'] = limit
+    # parameters['sysparm_fields'] = fieldsToReturn
+    parameters['sysparm_display_value'] = displayName
+    # query = raw_input('enter your query parameters: ')
+    parameters['sysparm_query'] = query   
+    response = requests.get(incUrl, auth=(user, pwd), headers=headers, params=parameters)
+    # print json.dumps(response.json(), indent=2)
+    if response.status_code == 200:
+        return response.json()
+
+
+def getIncidentInfo(incidents):
+    query = getQueryString(incidents)
+
+
+def getQueryString(queryList):
+    if queryList.length == 1:
+        return "" + queryList[0]
+    else:
+        queryString = "" + queryList[0]
+        for query in queryList[1:]:
+            queryString += "^OR" + query
+
+        return queryString
