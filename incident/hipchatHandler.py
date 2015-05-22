6cd95b1c8263b98489c0bd9f5858c17b572a4791
Gerard Van Halsema <gerardvh@me.com>
2015-05-22 18:07:00 -0400

    Moved config variable to hipchatHandler.py and added comments / edited for clarity

--- incident/hipchatHandler.py
+++ incident/hipchatHandler.py
@@ -1,8 +1,10 @@
-from incident_parser import messageToIncidentNumbers, incidentNumbersToLinks
+from service_link_request_handler import messageToIncidentNumbers, getIncidentJSONWithLinks
 from django.shortcuts import render
 import json
 
 
+# Beginnings of a json response to a HipChat webhook.
+# We will fill in the "message" variable below in handleHipchatMessage()
 hipChatReturnMessage = {
     "color": "green",
     "message": "",
@@ -10,15 +12,42 @@ hipChatReturnMessage = {
     "message_format": "html"
 }
 
+# HipChat add-on API expects a JSON response when installing that consists
+# of basic info about the webapp and a url to send POSTs to when the listener
+# is triggered by a match to the supplied pattern.
+config = {
+  "name": "SListener",
+  "description": "An add-on that listens for ServiceLink incidents \
+  and returns a structured and useful response to HipChat.",
+  "key": "com.example.myaddon",
+  "links": {
+    "homepage": "https://example.com/myaddon",
+    "self": "https://example.com/myaddon/capabilities"
+  },
+  "capabilities": {
+    "hipchatApiConsumer": {
+      "scopes": [
+        "send_notification"
+      ]
+    },
+    "webhook": [{
+      "url": "https://sl-listener.herokuapp.com/api/incident/",
+      "pattern": "((?:INC)+[0-9]{7})|((?:inc)+[0-9]{7})", # TODO: Change this to case-insensitive regex
+      "event": "room_message",
+      "name": "incident-debug"
+      }]
+  }
+}
+
 def handleHipchatMessage(message, request):
-    print "made it to handleHipchatMessage()"
-    # print json.dumps(message)
-    name = message['item']['message']['from']['name']
+    name = message['item']['message']['from']['name'] # full name of user
     nums = messageToIncidentNumbers(message['item']['message']['message'])
-    links, slResponseObj = incidentNumbersToLinks(nums)
-    print "about to try and render"
-    httpResponse = render(request, 'hipchat.html', {'name': name, 'incidents': slResponseObj})
-    print "successfully rendered"
-    hipChatReturnMessage['message'] = httpResponse.content
-    print "successfully added to message"
+    # Reduce the message text to just incident numbers (INC#######)
+    slResponseObj = getIncidentJSONWithLinks(nums)
+    httpResponse = render(request, 
+        'hipchat.html', # HTML template to use
+        {'name': name, # Pass in some variables to be used in our template
+        'incidents': slResponseObj}) 
+    hipChatReturnMessage['message'] = httpResponse.content 
+    # Just get the formatted HTML string for our HipChat message 
     return hipChatReturnMessage
