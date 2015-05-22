10996b376ca3296ec5b968f1798829728bc617a0
Gerard Van Halsema <gerardvh@me.com>
2015-05-22 18:13:09 -0400

    Edit for clarity and comments to urls.py

--- service_link_responder/urls.py
+++ service_link_responder/urls.py
@@ -7,8 +7,12 @@ urlpatterns = patterns('',
     # url(r'^$', 'service_link_responder.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', incident.views.index, name='index'),
+    url(r'^admin/', include(admin.site.urls)), # currently unused
+    url(r'^$', incident.views.index, name='index'), # currently unused
     url(r'^api/incident/', incident.views.incidentAPI, name='incidentAPI'),
+    # Receives requests to the base url + 'api/incident/' 
+    # runs incidentAPI() in views.py
     url(r'configuration$', incident.views.configuration, name='configuration')
+    # Receives requests to the base url + 'configuration'
+    # Returns a config json response for HipChat add-on installation
 )
